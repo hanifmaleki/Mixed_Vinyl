d106a03cc9a3dcc5c25c5b5eed0c8d0ca174007a
hanifmaleki <hanif_1362@yahoo.com>
2024-03-16 18:27:58 +0100

    Add first Twig template

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,13 +5,27 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('A simple message');
+        $tracks = [
+            ['song'=>'Track1', 'artist' => 'Coolio'],
+            ['song'=>'Track2', 'artist' => 'Coolio'],
+            ['song'=>'Track3', 'artist' => 'Coolio'],
+            ['song'=>'Track4', 'artist' => 'Coolio'],
+            ['song'=>'Track5', 'artist' => 'Coolio'],
+            ['song'=>'Track6', 'artist' => 'Coolio'],
+            ['song'=>'Track7', 'artist' => 'Coolio'],
+            ['song'=>'Track8', 'artist' => 'Coolio'],
+        ];
+        return $this->render('homepage.html.twig',[
+            'title' => 'PB  James',
+            'tracks' => $tracks,
+        ]);;
     }
 
     #[Route('/browse/{slug}')]
